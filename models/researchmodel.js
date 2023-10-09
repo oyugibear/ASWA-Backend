2401c964e0c56c3eae1728e7b897815c76e50b69
Arthur <arthur@Arthurs-Mac-mini.local>
2023-10-10 00:06:34 +0300

    editted the model

--- models/researchmodel.js
+++ models/researchmodel.js
@@ -15,7 +15,7 @@ const researchSchema = new mongoose.Schema({
     },
     type_of_presentation: {
         required: true,
-        type: Boolean
+        type: String
     },
     authors: {
         required: true,
